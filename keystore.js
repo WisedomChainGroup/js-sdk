f13c3e3f0c9b3908a5089e581dec52473dde9435
zhangtong <840043122@qq.com>
2019-07-18 16:41:20 +0800

    modify js-sdk

--- keystore.js
+++ keystore.js
@@ -10,6 +10,9 @@ const uuidV4 = require('uuid/v4');
 const path = __dirname + "/../../../keystore/main";
 const testpath = __dirname + "/../../../keystore/test";
 const bs58 = require('./base58');
+const Decimal = require('./decimal.js');
+const nacl = require('./nacl.min.js');
+const BN = require('bn.js');
 
 class KeyStore {
     constructor() {
@@ -113,7 +116,6 @@ class KeyStore {
         let time=new Date().getTime();
         const filePath = newpath + "/" + keystore.address+"@"+time;
         const content = JSON.stringify(keystore, null, 4);
-
         fs.writeFile(filePath, content, {flag: 'w'}, function (err) {
             if(err) {
                 console.error(err);
@@ -215,12 +217,48 @@ class KeyStore {
         return Buffer.from(hex, 'hex');
     }
 
-   addressToPubkeyHash(address){
+    addressToPubkeyHash(address){
         let _r5 = new bs58().decode(address);
         let r5 = this.buf2hex(_r5);
         let r2 = r5.substring(0,r5.length-8);
         let r1 = r2.substring(2,r2.length)
         return r1;
+    }
+
+    pubkeyHashToaddress(pubkeyHash){
+        let r1 = Buffer.from(pubkeyHash,'hex');
+        let r2 = "00"+pubkeyHash;
+        let a  = Buffer.from(r1, 'hex');
+        let b = keccak256(a);
+        let c =Buffer.from(b, 'hex');
+        let r3 = keccak256(c);
+        let b4 = r3.substring(0,8);
+        let r5 = r2+b4;
+        let r6 = new bs58().encode(this.Hex2Array(r5));
+        return  r6;
+    }
+
+    verifyAddress(address){
+        if(address==""||address==null){
+            return -1;
+        }
+        if(address.substring(0,1) == 1){
+            let _r5 = new bs58().decode(address);
+            let a = Buffer.from(this.addressToPubkeyHash(address),'hex');
+            let b = keccak256(a)
+            let c =Buffer.from(b, 'hex');
+            let r3 = keccak256(c);
+            let b4 = r3.substring(0,8);
+            let _b4 = this.Bytes2Str(_r5).substring(42,50);
+            if(b4 == _b4){
+                return 0;
+            }else{
+                return -2;
+            }
+
+        }else{
+            return -1;
+        }
     }
 
     prikeyToPubkey(prikey){
@@ -232,6 +270,81 @@ class KeyStore {
     async keystoreToPubkey(keyStore,pwd){
         return this.prikeyToPubkey(await this.DecryptSecretKeyfull(keyStore, pwd));
 
+    }
+
+    Bytes2Str(arr){
+        var str = "";
+        for(var i=0; i<arr.length; i++){
+            var tmp = arr[i].toString(16);
+                if(tmp.length == 1){
+                    tmp = "0" + tmp;
+                }
+            str += tmp;
+        }
+        return str;
+    }
+
+    encodeUint32(value){
+        const buf = Buffer.alloc(4);
+        buf[0] = ((value & 0x00000000FF000000) >>> 24);	
+        buf[1] = ((value & 0x0000000000FF0000) >>> 16);
+        buf[2] = ((value & 0x000000000000FF00) >>> 8);
+        buf[3] = (value &  0x00000000000000FF);
+        return buf;
+    }
+
+
+    encodeUint64(value){
+	    const buf = Buffer.alloc(8);
+	    buf[0] = ((value & 0xFF00000000000000) >>> 56);
+        buf[1] = ((value & 0x00FF000000000000) >>> 48);
+        buf[2] = ((value & 0x0000FF0000000000) >>> 40);
+        buf[3] = ((value & 0x000000FF00000000) >>> 32);
+        buf[4] = ((value & 0x00000000FF000000) >>> 24);
+        buf[5] = ((value & 0x0000000000FF0000) >>> 16);
+        buf[6] = ((value & 0x000000000000FF00) >>> 8);
+        buf[7] = (value &  0x00000000000000FF);
+	    return buf;
+    }
+
+    ClientToTransferAccount(fromPubkeyStr,toPubkeyHashStr,amount,prikeyStr,nonce){
+        //版本号
+        let version="01";
+        //类型：WDC转账
+        let type="01";
+        //Nonce 无符号64位
+        let nonece=this.Bytes2Str(this.encodeUint64(nonce+1));
+        //签发者公钥哈希 20字节
+        let fromPubkeyHash = fromPubkeyStr;
+        //gas单价
+        let gasPrice = this.Bytes2Str(this.encodeUint64(4));
+        //转账金额 无符号64位
+        let rate=100000000;
+        let bdAmount = new Decimal(amount).mul(new Decimal(rate).toNumber());
+        let Amount=this.Bytes2Str(this.encodeUint64(bdAmount));
+        //为签名留白
+        const buffersignull = Buffer.alloc(64);
+        let signull = this.Bytes2Str(buffersignull);
+        //接收者公钥哈希
+        let toPubkeyHash=toPubkeyHashStr;
+        //长度
+        let allPayload= "00000000";
+        let RawTransaction=Buffer.from(version+type+nonece+fromPubkeyHash+gasPrice+Amount+signull+toPubkeyHash+allPayload, 'hex');
+        let a = version+type+nonece+fromPubkeyHash+gasPrice+Amount+signull+toPubkeyHash+allPayload;
+        //签名数据
+        let secretKey = Buffer.from(prikeyStr+fromPubkeyStr, 'hex');
+        let sigall = nacl.sign(RawTransaction,secretKey);
+        let sigHex = this.Bytes2Str(sigall).substring(0,128);
+        let _tra = version+type+nonece+fromPubkeyHash+gasPrice+Amount+sigHex+toPubkeyHash+allPayload;
+        let tra = Buffer.from(_tra,'hex');
+        let transha = keccak256(tra);
+        let signRawBasicTransaction = version+transha+type+nonece+fromPubkeyHash+gasPrice+Amount+sigHex+toPubkeyHash+allPayload;
+        return {
+            'txHash': transha,
+            'transaction': signRawBasicTransaction,
+            "a":a,
+            "v":transha
+        }
     }
 
     Check() {
--- test.js
+++
@@ -1,9 +0,0 @@
-const Keystore = require('./keystore');
-const keystore = new KeyStore();
-let passwd = "1111111";
-let net = "1";
-//创建keystore
-async function add(){
-const savefile = await keystore.Create(passwd,net);
-}
-
