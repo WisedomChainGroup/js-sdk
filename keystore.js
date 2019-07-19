10c618313ab2e35d6723d7335614d231c3a626fc
zhangtong <840043122@qq.com>
2019-07-19 14:56:29 +0800

    add modifypassword

--- keystore.js
+++ keystore.js
@@ -20,6 +20,9 @@ class KeyStore {
 
     async Create (pwd) {
         try {
+            if(pwd.length>20 || pwd.length<8){
+                return -1;
+            }
             let keyStore = {};
             const account = new AccountHandle().createAccount();
             //地址
@@ -182,7 +185,6 @@ class KeyStore {
                     }
                     str += tmp;
                 }
-
             return str;
         }catch (error) {
             return 5000;   
@@ -289,6 +291,66 @@ class KeyStore {
             return this.buf2hex(keyPair.publicKey);
         } catch (error) {
             return 5000;   
+        }
+    }
+
+    async modifyPassword(ks,pwd,newpwd){
+        try{
+            if(pwd.length>20 || pwd.length<8){
+                return -1;
+            }
+            let _prikey = await this.DecryptSecretKeyfull(ks, pwd);
+            let keyStore = {};
+            const account = new AccountHandle().createAccount();
+            //地址
+            keyStore.address = account.addr;
+            keyStore.crypto = {};
+            //使用的加密算法，默认为aes-256-ctr
+            keyStore.crypto.cipher = "aes-256-ctr";
+            //keyStore.crypto.ciphertext = "";
+            keyStore.crypto.cipherparams = {};
+            //算法所需的参数，随机生成
+            keyStore.crypto.cipherparams.iv = crypto.randomBytes(16).toString('hex');  // must be 128 bit, random 
+
+            //const aesCtr = new aesjs.ModeOfOperation.ctr(key_256, new aesjs.Counter(5));
+            //var encryptedBytes = aesCtr.encrypt(textBytes);
+            //密钥加密方法
+            keyStore.kdf = "Argon2id";
+            //Argon2id的参数，分别是散列计算的迭代次数，必须使用的存储器的大小以及可以并行计算散列的CPU数量
+            keyStore.kdfparams = {};
+            keyStore.kdfparams.timeCost = 4;
+            keyStore.kdfparams.memoryCost = 20480;
+            keyStore.kdfparams.parallelism = 2;
+            //Argon2id哈希计算使用的盐值，随机生成32
+            keyStore.kdfparams.salt = crypto.randomBytes(32).toString('hex'); // random
+            //keystore格式的版本号，默认为1
+            keyStore.version = "1";
+            //私钥加密
+            const salt = Buffer.from(keyStore.kdfparams.salt, 'hex');
+            const options = {
+                timeCost: 4, memoryCost: 20480, parallelism: 2, type: argon2.argon2id, hashLength: 32, 
+                version: 0x13, raw: true, salt
+            };
+            const p1 = Buffer.from(newpwd, 'ascii').toString('hex');
+            const s1 = keyStore.kdfparams.salt + p1;
+            const derivedKey = await argon2.hash(s1, options);
+
+            const vi = Buffer.from(keyStore.crypto.cipherparams.iv, 'hex');
+            const aesCtr = new aesjs.ModeOfOperation.ctr(derivedKey, new aesjs.Counter(vi));
+            let prikey = Buffer.from(_prikey,'hex');
+            const encryptedBytes = aesCtr.encrypt(prikey);
+            //加密过的私钥
+            keyStore.crypto.ciphertext = aesjs.utils.hex.fromBytes(encryptedBytes);
+
+            //用来比较解密密钥与口令的
+            const dc = derivedKey.toString('hex') + keyStore.crypto.ciphertext;
+            const dc_buf = Buffer.from(dc, 'hex');
+            keyStore.mac = keccak256(dc_buf);
+            //这是UUID，可以直接通过程序计算得到
+            keyStore.id = uuidV4();
+            return keyStore;
+        }catch(error){
+            return 5000;
         }
     }
 
