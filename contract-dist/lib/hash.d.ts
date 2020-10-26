8adcdbcb739e05b4d2d875f17380b6152849b8a8
zhuyingjie <m6567fc@outlook.com>
2020-10-26 15:45:18 +0800

    remove unused

--- contract-dist/lib/hash.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class Hash {
-    private static hash;
-    static keccak256(data: ArrayBuffer): ArrayBuffer;
-}
--- contract-dist/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function log(a: string): void;
-export * from './context';
-export * from './rlp';
-export * from './db';
-export * from './safemath';
-export * from './hash';
-export * from './util';
